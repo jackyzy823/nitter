e4602b04e057a2882833b0298a1c4b5dd1c558b3
jackyzy823 <jackyzy823@gmail.com>
2024-02-24 18:44:26 +0800

    Support .vtt in media route

--- src/routes/media.nim
+++ src/routes/media.nim
@@ -178,4 +178,8 @@ proc createMediaRouter*(cfg: Config) =
         let vid = await safeFetch(url)
         content = proxifyVideo(vid, requestPrefs().proxyVideos, url)
 
+      if ".vtt" in url:
+        # TODO Or set enableWebVTT : false to new Hls in hlsPlayback.js
+        content = await safeFetch(url)
+
       resp content, m3u8Mime
